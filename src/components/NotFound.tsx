706128ef9decda5308472ac7679e9eb85dc67d5b
Lucerna <83871124+imosmdrk58@users.noreply.github.com>
2025-06-22 22:38:32 +0300

    Update NotFound.tsx

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,8 +1,8 @@
 const NotFound = () => {
   return (
     <main className="w-full h-96 flex flex-col items-center justify-center text-[#444444] dark:text-[#9ca9b9]">
-      <h1 className="text-3xl font-bold">404 Not Found</h1>
-      <p>This page could not be found. Please check the URL</p>
+      <h1 className="text-3xl font-bold">404 Bulunamadı</h1>
+      <p>Bu sayfa bulunamadı. Lütfen URL’yi kontrol edin.</p>
     </main>
   )
 }
